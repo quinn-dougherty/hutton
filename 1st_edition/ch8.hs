ff29ede487441cbf81bbcaac6c5ecf255eface73
Quinn Dougherty <quinnd@riseup.net>
2018-05-22 00:02:56 -0400

    nothign to write home about

--- 1st_edition/ch8.hs
+++ 1st_edition/ch8.hs
@@ -41,7 +41,84 @@ p = do x <- item
        y <- item
        return (x,y)
 
+choice := apply first parser to input string, and if it fails apply the second one.
+
+p +++ q is read "p or else q".
+
+(+++) :: Parser a -> Parser a -> Parser a
+p +++ q = \inp -> case parse p inp of
+                       [] -> parse q inp
+                       [(v,out)] -> [(v,out)]
+
+sat :: (Char -> Bool) -> Parser Char
+sat p = do x <- item
+          if p x then return x else failure
+
+-- here, w sat, we can do things w "isDigit, isLower, isUpper, isAlphaNum" etc. 
+
+string :: String -> Parser String
+string [] = return []
+string (x:xs) = do char x
+                   string xs
+                   return (x:xs)
 
 
+many :: Parser a -> Parser [a]
+many p = many1 p +++ return []
 
+many1 :: Parser a -> Parser [a]
+many1 p = do v <- p
+             vs <- many p
+             return (v:vs)
+
+ident :: Parser String
+ident = do x <- lower
+           xs <- many alphanum
+           return (x:xs)
+
+-- grammar notes:
+expr ::= expr + expr | expr * expr | (expr) | nat
+nat ::= 0 | 1 | 2 | ...
+
+-- this is arithmetic is ambiguous because of 2 * 3 + 4.
+
+-- this solves the value ambiguity because it expresses order of operations, but it does not solve tree ambiguity because 1 + 2 + 3, while having the same value, has 2 different trees depending on assoc. 
+expr ::= expr + expr | term
+term ::= term * term | factor
+factor ::= (expr) | nat
+nat ::= 0 | 1 | 2 | ...
+
+-- This one is fully disambiguated:
+nat ::= 0 | 1 | 2 | ...
+factor ::= (expr) | nat
+term ::= factor * term | factor
+expr ::= term + expr | term
+
+-- a simplicication: 
+expr ::= term (+ expr | [] )
+term ::= factor (* term | [] )
+factor ::= (expr) | nat
+nat ::= 0 | 1 | 2 | ...
+--where [] is epsilon or "" empty string.
+
+-- if our inventory parsers were implemented, here it would be in literate haskell code: 
+expr :: Parser Int
+expr = do t <- term
+          do symbol "+"
+             e <- expr
+             return (t + e)
+           +++ return t
+term :: Parser Int
+term = do f <- factor
+          do symbol "*"
+             t <- term
+             return (f * t)
+            +++ return f
+factor :: Parser Int
+factor = do symbol "("
+            e <- expr
+            symbol ")"
+            return e
+           +++ natural
 -} 
+
--- 2nd_edition/ch4.hs
+++ 2nd_edition/ch4.hs
@@ -1,7 +1,52 @@
 --4.8.1
-halve' :: [a] -> ([a],[a])
-halve' xs = (tops,bottoms)
-  where
-    tops <- take n xs
-    bottoms <- drop n xs
-    n <- div (length xs) 2
+halve' :: [a] -> Maybe ([a],[a])
+halve' xs | even (length xs) = Just (tops,bottoms)
+          | otherwise = Nothing
+ where
+   tops    = take n xs
+   bottoms = drop n xs
+   n       = div (length xs) 2
+
+--4.8.2
+thirdA :: [a] -> a -- w head and tail
+thirdA xs = head (tail (tail xs))
+
+thirdB :: [a] -> a -- w list indexing
+thirdB xs = xs !! 2
+
+thirdC :: [a] -> a -- w pattern matching. 
+thirdC (x:xs) = x -- damn i have no idea how to do this
+-- ^^^ not a solution
+
+safeTailA :: Eq a => [a] -> [a] -- conditional expression
+safeTailA (x:xs) = if (x:xs) == [] then [] else xs
+-- hrm again here we can't check empty list equality w/o asserting that a is an Eq a! there ought to be a way around this
+-- Ok this currently gives ""*** Exception: ch4.hs:22:1-50: Non-exhaustive patterns in function safeTailA"" when you enter safeTailA []. hrm.
+
+safeTailB :: Eq a => [a] -> [a] -- guard
+safeTailB (x:xs) | (x:xs) == [] = []
+                 | otherwise    = xs
+-- same non-exhaustive pattern issue.
+
+-- just for fun-- 
+safeTailMonad :: (Eq a) => [a] -> Maybe [a]
+safeTailMonad (x:xs) | (x:xs) == [] = Nothing
+                     | otherwise    = Just xs
+-- the problem w patterns here is certainly w "(x:xs) == []"
+
+-- gonna have to actually read the chapter
+
+--SafeTailC :: [a] -> [a] -- patternMatching.
+
+
+-- 4.8.7
+mult3 :: Int -> Int -> Int -> Int
+mult3 x y z = (\p -> (\q -> (\r -> p*q*r) z) y) x
+
+-- 4.8.8
+luhnDouble :: Int -> Int
+luhnDouble x | x * 2 > 9 = x*2 - 9
+             | otherwise = x*2
+
+luhn :: Int -> Int -> Int -> Int -> Bool
+luhn w x y z = (mod (sum [luhnDouble w, x, luhnDouble y, z]) 10) == 0
