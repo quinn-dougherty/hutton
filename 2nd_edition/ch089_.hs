0c234f4cce29357b89e67430d8cf79c9101e0743
Quinn Dougherty <quinnd@riseup.net>
2018-06-02 03:29:07 -0400

    8.9.1 totally done. some progress on 8.9.2 and 8.9.3

--- 2nd_edition/ch089_.hs
+++ 2nd_edition/ch089_.hs
@@ -18,9 +18,7 @@ one = Succ Zero
 mult :: Nat -> Nat -> Nat
 mult Zero n = Zero
 -- mult one n = n
-mult (Succ n) m = add n (mult n m) {- somehow
-*Main> multChannel 4 5
-6  -- i don't understand -- i'll have to write down the evaluation when i get off this plane    -} 
+mult (Succ n) m = add m (mult n m) {- this suddenly worked. Don't fully see how all my previous attempts weren't working.   -} 
 
 addChannel :: Int -> Int -> Int
 addChannel x y = nat2int (add (int2nat x) (int2nat y))
@@ -29,9 +27,9 @@ multChannel x y = nat2int (mult (int2nat x) (int2nat y))
 
 --8.9.2 -- redefine occurs for search trees instead of regular trees. 
 
-data SearchTree a = Ord a => Leaf a | Node (SearchTree a) a (SearchTree a) deriving Show
+data SearchTree a = Leaf a | Node (SearchTree a) a (SearchTree a) deriving Show -- tried to do this w "Ord a => " but it said needed to be existentially quantified (not a forall) and something about GADTs 
 
-occurs' :: Ord a => a -> Tree a -> Bool -- from the text
+occurs' :: Ord a => a -> SearchTree a -> Bool -- from the text
 occurs' x (Leaf y)                 = x==y
 occurs' x (Node l y r) | x==y      = True
                        | x <y      = occurs' x l
@@ -46,11 +44,20 @@ occurs x (Node l y r) = False
 data BinTree a = LeafBin a | NodeBin (BinTree a) (BinTree a) deriving Show
 
 numLeaves :: BinTree a -> Int
-numLeaves LeafBin x = nat2int one
-numLeaves NodeBin t1 t2 = nat2int (add (numLeaves t1) (numLeaves t2)) -- i believe this is correct. 
+numLeaves (LeafBin x) = nat2int one
+numLeaves (NodeBin t1 t2) = ((+) (numLeaves t1) (numLeaves t2)) -- i believe this is correct. 
 
 balanced :: BinTree a -> Bool
-balanced LeafBin x = True
-balanced NodeBin t1 t2 | numLeaves t1 == numLeaves t2 = True
-                       | otherwise = False -- i know, doesn't account for "off by one" for odd numbers, yet... 
+balanced (LeafBin x)                                    = True -- singleton or empty trees are balanced
+balanced (NodeBin t1 t2) | numLeaves t1     == numLeaves t2 = True
+                         | 1 + numLeaves t1 == numLeaves t2 = True
+                         | numLeaves t1 == 1 + numLeaves t2 = True
+                         | otherwise                        = False -- i know, doesn't account for "off by one" for odd numbers, yet... 
 
+height :: BinTree a -> Int
+height (LeafBin x) = 1
+height (NodeBin t1 t2) = 1 + (max (height t1) (height t2)) -- i think this is correct. 
+
+--trash data to test this
+t :: BinTree Int
+t = NodeBin (NodeBin (NodeBin (LeafBin 1) (LeafBin 0)) (LeafBin 2)) (NodeBin (LeafBin 1) (LeafBin 3)) -- this got a numLeaves 5, currently height 4, and not balanced. and balanced True (its the odd case, the last True case before otherwise. 
