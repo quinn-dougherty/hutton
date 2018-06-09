83419ab927a5a7aeed9c9911dfc81e7bd4448baa
Quinn Dougherty <quinnd@riseup.net>
2018-06-08 20:07:07 -0700

    finished exercise 1

--- 2nd_edition/ch09.hs
+++ 2nd_edition/ch09.hs
@@ -46,19 +46,27 @@ ex' = App Mul (App Add (Val 1) (Val 50)) (App Sub (Val 25) (Val 10))
 
 subs :: [a] -> [[a]]
 subs [] = [[]]
-subs (x:xs) = yss ++ map (x:) yss
+subs (x:xs) = yss ++ map (x:) yss -- 2^n in input length. x
               where yss = subs xs
 
 interleave :: a -> [a] -> [[a]]
-interleave x []     = [[x]]
+interleave x []     = [[x]] -- n+1 in input length 
 interleave x (y:ys) = (x:y:ys) : map (y:) (interleave x ys)
 
 perms :: [a] -> [[a]]
-perms []     = [[]]
+perms []     = [[]] -- factorial in input length
 perms (x:xs) = concat (map (interleave x) (perms xs))
 
 choices :: [a] -> [[a]]
-choices = concat . map perms . subs
+choices = concat . map perms . subs -- This growth is:
+{- length xs  =>   choices xs
+     1                  2
+     2                  5
+     3                  16
+     4                  65
+     5                 326
+-- i don't understand this function
+-}
 
 solution :: Expr -> [Int] -> Int -> Bool
 solution e ns n =
@@ -114,8 +122,20 @@ solutions'' :: [Int] -> Int -> [Expr]
 solutions'' ns n =
   [e | ns' <- choices ns, (e,m) <- results ns', m == n]
 
+
 main :: IO () -- i can't abstract this out into another file like it says in the book
 main = print (solutions'' [1,3,7,10,25,50] 765)
 -- solutions' is supposed to be optimized pretty well
 
--- further optimzation by going up to valid and alterning it for commutativity 
+-- further optimzation by going up to valid and altering it for commutativity 
+
+--- EXERCISES ---
+-- 9.11.1 -- redefine the combintatorial fnction choices using a list comprehension rather than using composition, concat, map.
+
+choices' :: [a] -> [[a]]
+choices' xs = concat [perms ys | ys <- (subs xs)]
+-- hrm it doesn't want me to use concat tho.
+
+-- 9.11.2 -- isChoice :: Eq a => [a] -> [a] -> Bool
+-- which decides if one list is chosen from another
+--- w/o using perms and subs.
