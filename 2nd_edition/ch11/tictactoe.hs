2402ec509a2b29b1cd0c49ee4ce0198d914bc891
Quinn Dougherty <quinn.dougherty92@gmail.com>
2019-10-30 12:08:44 -0400

    begin tictactoe

---
+++ 2nd_edition/ch11/tictactoe.hs
@@ -0,0 +1,117 @@
+import Data.Char
+import Data.List
+import System.IO
+
+size :: Int
+size = 3
+
+data Player = O | B | X
+            deriving (Eq, Ord, Show)
+type Grid = [[Player]]
+
+next :: Player -> Player
+next O = X
+next B = B
+next X = O
+
+empty :: Grid
+empty = replicate size (replicate size B)
+
+full :: Grid -> Bool
+full = all (/= B) . concat
+
+turn :: Grid -> Player
+turn g = if os <= xs then O else X
+  where
+    os = length (filter (== O) ps)
+    xs = length (filter (== X) ps)
+    ps = concat g
+
+diag :: Grid -> [Player]
+diag g = [g !! n !! n | n <- [1..size-1]]
+
+wins :: Player -> Grid -> Bool
+wins p g = any line (rows ++ cols ++ dias)
+  where
+    line = all (== p)
+    rows = g
+    cols = transpose g
+    dias = [diag g, diag (map reverse g)]
+
+won :: Grid -> Bool
+won g = wins O g || wins X g
+
+-- displaying a grid
+showPlayer :: Player -> [String]
+showPlayer O = ["   ", " O ", "   "]
+showPlayer B = ["   ", "   ", "   "]
+showPlayer X = ["   ", " X ", "   "]
+
+interleave :: a -> [a] -> [a]
+interleave x []     = []
+interleave x [y]    = [y]
+interleave x (y:ys) = y : x : interleave x ys
+
+showRow :: [Player] -> [String]
+showRow = beside . interleave bar . map showPlayer
+  where
+    beside = foldr1 (zipWith (++))
+    bar = replicate 3 "|"
+
+putGrid :: Grid -> IO ()
+putGrid =
+  putStrLn . unlines . concat . interleave bar . map showRow
+  where bar = [replicate ((size*4)-1) '-']
+
+-- making a move.
+valid :: Grid -> Int -> Bool
+valid g i = 0 <= i && i < size^2 && concat g !! i == B
+
+chop :: Int -> [a] -> [[a]]
+chop n [] = []
+chop n (x:xs) = take n xs : chop n (drop n xs)
+
+move :: Grid -> Int -> Player -> [Grid]
+move g i p =
+  if valid g i then [chop size (xs ++ [p] ++ ys)] else []
+  where (xs, B:ys) = splitAt i (concat g)
+
+-- get move from player
+getNat :: String -> IO Int
+getNat prompt = do putStr prompt
+                   xs <- getLine
+                   if xs /= [] && all isDigit xs then
+                     return (read xs)
+                   else
+                     do putStrLn "ERROR: Invalid Number"
+                        getNat prompt
+
+-- main
+cls :: IO ()
+cls = putStr "\ESC[2J"
+
+goto :: (Int, Int) -> IO ()
+goto (x,y) = putStr ("\ESC[" ++ show y ++ ";" ++ show x ++ "H")
+
+prompt :: Player -> String
+prompt p = "Player " ++ show p ++ ", enter your move: "
+
+run' :: Grid -> Player -> IO ()
+run' g p | wins O g  = putStrLn "Player O wins!\n"
+         | wins X g  = putStrLn "Player X wins!\n"
+         | full g    = putStrLn "It's a draw"
+         | otherwise =
+             do i <- getNat (prompt p)
+                case move g i p of
+                  [] -> do putStrLn "ERROR: Invalid move"
+                           run' g p
+                  [g'] -> run g' (next p)
+
+run :: Grid -> Player -> IO ()
+run g p = do cls
+             goto (1,1)
+             putGrid g
+             run' g p
+
+tictactoe :: IO ()
+tictactoe = run empty O
